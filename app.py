40b0f155d7f4fd7667aae227eaa5bc1c4092fbac
johnfbeltranc <beltranjhon463@gmail.com>
2025-12-11 21:33:13 +0000

    se realiza la app para desplegar en render

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import pickle
 import pandas as pd
 
 app = Flask(__name__)
-with open("../models/iris_model.pkl", "rb") as file:
+with open("iris_model.pkl", "rb") as file:
     artifact = pickle.load(file)
     
 
 @app.route("/", methods=["GET","POST"])
 def prediction():
-    if request.methods == "POST":
+    if request.method == "POST":
         val1 = float(request.form["val1"])
         val2 = float(request.form["val2"])
         val3 = float(request.form["val3"])
@@ -18,10 +18,10 @@ def prediction():
 
         df_predict = pd.DataFrame([[val1, val2, val3, val4]], columns=artifact["predictors"])
         result = artifact["model"].predict(df_predict)
-        predicted_label = artifact["label_encoder"].inverse_transform(result)
+        predicted_label = artifact["target_encoder"].inverse_transform(result)
     else:
         predicted_label = None
-    return render_template("./index.html", prediction=predicted_label)
+    return render_template("index.html", prediction=predicted_label)
     
 
 
